236fc7724bcd18be9acaba2ee99915a9a2be21f6
dnapolet <dnapolet@our.ecu.edu.au>
2021-08-17 10:31:30 +0000

    Adding Week 1 scripts to portfolio

---
+++ portfolio/week1/first.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+
+#title           :first.sh
+#description     :This test script will print a simple greeting to the screen.
+#author		     :Damien Napoletano
+#date            :13/07/2021
+#version         :1.1
+
+# Start of script
+
+
+
+    echo "Hi there!" # Print "Hi there!" to the screen.
+      
+    exit 0 # Exit with a code of '0' meaning successfully executed.
+
+
+
+# End of script
---
+++ portfolio/week1/second.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#title           :second.sh
+#description     :This script will receive input from the user and display it as part of the screen output.
+#author		     :Damien Napoletano
+#date            :13/08/2021
+#version         :1.1
+
+# Start of script
+
+
+
+    echo "Hi there!" # Print 'Hi there!' to screen
+    echo "It's good to see you $1" # Print 'It's good to see you '$1', where $1 is replaced with the user input.
+    exit 0 # Exit with a success code of '0'.
+
+
+# End of script
